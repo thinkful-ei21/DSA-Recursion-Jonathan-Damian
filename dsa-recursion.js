f1d68fbb070d707c136e2b3933c713a2e653dd76
BuddhaL1T3 <jmriggs75@gmail.com>
2018-07-23 19:14:48 -0400

    finished the recursive part and used solutions to compare and modify for error resolution

--- dsa-recursion.js
+++ dsa-recursion.js
@@ -132,18 +132,144 @@ const factorialRec = num => {
 
 // recursive
 const fibonacciRec = num => {
-  const fibSeqArr = [1, 1];
-  if (fibSeqArr.length === num) {
-    return fibSeqArr;
+  if (num === 0) {
+    return 0;
   }
-
-  let nextNum =
-    fibSeqArr[fibSeqArr.length - 2] + fibSeqArr[fibSeqArr.length - 1];
-  fibSeqArr.push(nextNum);
-  return [...fibSeqArr, ...fibonacciRec(num - 1)];
+  if (num === 1) {
+    return 1;
+  }
+  return fibonacciRec(num - 1) + fibonacciRec(num - 2);
 };
 
-console.log(fibonacciRec(7));
+// console.log(fibonacciRec(3));
 
+// non-recursive
+// do ^ here
+
+/////////////////// ANAGRAMS ///////////////////
+
+// recursive
+const anagramsRec = (prefix, str) => {
+  if (str.length <= 1) {
+    console.log(`the anagram is ${prefix}${str}`);
+  } else {
+    for (let i = 0; i < str.length; i++) {
+      let currentLetter = str.substring(i, i + 1);
+      let prevLetters = str.substring(0, i);
+      let subsequentLetters = str.substring(i + 1);
+      anagramsRec(prefix + currentLetter, prevLetters + subsequentLetters);
+    }
+  }
+};
+
+// console.log(anagramsRec('', 'ab'));
+
+// non-recursive
+// do ^ here
+
+/////////////////// ANIMAL HIERARCHY ///////////////////
+
+// recursive
+
+const animalHierarchy = [
+  { id: 'Animals', parent: null },
+  { id: 'Mammals', parent: 'Animals' },
+  { id: 'Dogs', parent: 'Mammals' },
+  { id: 'Cats', parent: 'Mammals' },
+  { id: 'Golden Retriever', parent: 'Dogs' },
+  { id: 'Husky', parent: 'Dogs' },
+  { id: 'Bengal', parent: 'Cats' }
+];
+
+// ==============================
+function traverse(animalHierarchy, parent) {
+  let node = {};
+  animalHierarchy
+    .filter(item => item.parent === parent)
+    .forEach(item => (node[item.id] = traverse(animalHierarchy, item.id)));
+  return node;
+}
+// console.log(traverse(animalHierarchy, null));
+
+// non-recursive
+// do ^ here
+
+/////////////////// ORGANIZATION CHART ///////////////////
+
+// recursive
+let organization = {
+  Zuckerberg: {
+    Schroepfer: {
+      Bosworth: {
+        Steve: {},
+        Kyle: {},
+        Andra: {}
+      },
+      Zhao: {
+        Richie: {},
+        Sofia: {},
+        Jen: {}
+      },
+      Badros: {
+        John: {},
+        Mike: {},
+        Pat: {}
+      },
+      Parikh: {
+        Zach: {},
+        Ryan: {},
+        Tes: {}
+      }
+    },
+    Schrage: {
+      VanDyck: {
+        Sabrina: {},
+        Michelle: {},
+        Josh: {}
+      },
+      Swain: {
+        Blanch: {},
+        Tom: {},
+        Joe: {}
+      },
+      Frankovsky: {
+        Jasee: {},
+        Brian: {},
+        Margaret: {}
+      }
+    },
+    Sandberg: {
+      Goler: {
+        Eddie: {},
+        Julie: {},
+        Annie: {}
+      },
+      Hernandez: {
+        Rowi: {},
+        Inga: {},
+        Morgan: {}
+      },
+      Moissinac: {
+        Amy: {},
+        Chuck: {},
+        Vinni: {}
+      },
+      Kelley: {
+        Eric: {},
+        Ana: {},
+        Wes: {}
+      }
+    }
+  }
+};
+
+function goThroughChart(node, indent = 0) {
+  for (let key in node) {
+    console.log(' '.repeat(indent), key);
+    goThroughChart(node[key], indent + 4);
+  }
+}
+
+console.log(goThroughChart(organization));
 // non-recursive
 // do ^ here
